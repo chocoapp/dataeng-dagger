10a9f154af512812bd5fb9095b40ae5ac656a36a
removedporn <86824510+removedporn@users.noreply.github.com>
2023-02-09 08:48:36 +0800

    Delete dagger directory

--- dagger/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for dagger."""
-
-__author__ = """David Siklosi"""
-__email__ = "siklosi.david@gmail.com"
-__version__ = "0.1.0"
--- dagger/pipeline/tasks/dummy_task.py
+++
@@ -1,8 +0,0 @@
-from dagger.pipeline.task import Task
-
-
-class DummyTask(Task):
-    ref_name = "dummy"
-
-    def __init__(self, name, pipeline_name, pipeline, job_config):
-        super().__init__(name, pipeline_name, pipeline, job_config)
